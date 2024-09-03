0232f58cd7c66707a0c854a5f8afe2e2773be591
George Miao <gm@miao.dev>
2024-09-03 01:49:14 -0400

    Update

--- unit/home/git.nix
+++ unit/home/git.nix
@@ -1,11 +1,11 @@
-{flake, ...}: {
+{...}: {
   programs.git = {
     enable = true;
 
     userName = "George Miao";
     userEmail = "gm@miao.dev";
 
-    difftastic = {enable = true;};
+    difftastic.enable = true;
 
     signing = {
       key = null;
@@ -15,11 +15,6 @@
     extraConfig = {
       init.defaultBranch = "main";
       tag.gpgSign = true;
-      # url = {
-      #   "https://oauth2:${flake.self.secrets.github.oauth_token}@github.com" = {
-      #     insteadOf = "https://github.com";
-      #   };
-      # };
     };
   };
 }
