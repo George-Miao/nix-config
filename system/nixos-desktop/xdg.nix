13130c23ceb3617640cebf9edf124d9a52b4e0b1
George Miao <gm@miao.dev>
2024-09-27 17:31:22 -0400

    Update xdg

--- system/nixos-desktop/xdg.nix
+++ system/nixos-desktop/xdg.nix
@@ -5,11 +5,13 @@
       defaultApplications = {
         "image/x-sony-arw" = "org.gnome.eog.desktop";
         "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
+        "application/pdf" = "org.gnome.Evince.desktop";
       };
       associations = {
         added = {
           "image/x-sony-arw" = "org.gnome.eog.desktop";
           "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
+          "application/pdf" = "org.gnome.Evince.desktop";
         };
       };
     };
