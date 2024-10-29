1613d7019fc5c7b3f613057b25a10a44721b9363
George Miao <gm@miao.dev>
2024-10-29 10:37:30 -0400

    Update

--- unit/home/rbw.nix
+++ unit/home/rbw.nix
@@ -2,9 +2,14 @@
   secrets,
   pkgs,
   ...
-}: {
+}: let
+  pinentry =
+    if builtins.match pkgs.system "darwin"
+    then pkgs.pinentry_mac
+    else pkgs.pinentry-qt;
+in {
   programs.rbw = {
     enable = true;
-    settings = {pinentry = pkgs.pinentry-qt;} // secrets.bitwarden;
+    settings = {inherit pinentry;} // secrets.bitwarden;
   };
 }
