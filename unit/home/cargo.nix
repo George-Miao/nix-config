dd7962ffd48e46f0f235cd1ed2e6151e0453847b
George Miao <gm@miao.dev>
2024-10-30 12:43:01 -0400

    Update

--- unit/home/cargo.nix
+++ unit/home/cargo.nix
@@ -1,9 +1,17 @@
-{...}: {
+{secrets, ...}: {
   home.file.cargo_config = {
     target = ".cargo/config.toml";
     text = ''
       [build]
       target-dir = "/tmp/rust-target"
+    '';
+  };
+
+  home.file.cargo_credential = {
+    target = ".cargo/credentials.toml";
+    text = ''
+      [registry]
+      token = "${secrets.cargo.token}"
     '';
   };
 }
