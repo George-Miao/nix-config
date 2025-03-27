f8038b1536256734c116eee9bf153fb7282f8cbb
nvfetcher action <action@miao.dev>
2025-03-27 12:25:17 +0000

    Update generated dependencies

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -26,10 +26,10 @@
   };
   vscode-darwin-insider = {
     pname = "vscode-darwin-insider";
-    version = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/87a35e10699cb846c2c72eeb3a698bf56dafd701/VSCode-darwin-universal.zip";
+    version = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/ff52a0da2eeb3d4c590a85cf14ca13a9d6c1b2cd/VSCode-darwin-universal.zip";
     src = fetchTarball {
-      url = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/87a35e10699cb846c2c72eeb3a698bf56dafd701/VSCode-darwin-universal.zip";
-      sha256 = "sha256-OVNvksnKpZ/ou+cPG4aEf5Wk3darf27LBdVKMLLe17g=";
+      url = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/ff52a0da2eeb3d4c590a85cf14ca13a9d6c1b2cd/VSCode-darwin-universal.zip";
+      sha256 = "sha256-BC0TE+3reYL2zEBJRtguvRUNX11gOV63mfzLHQryltI=";
     };
   };
   vscode-darwin-stable = {
@@ -42,10 +42,10 @@
   };
   vscode-linux-insider = {
     pname = "vscode-linux-insider";
-    version = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/87a35e10699cb846c2c72eeb3a698bf56dafd701/code-insider-x64-1742966545.tar.gz";
+    version = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/ff52a0da2eeb3d4c590a85cf14ca13a9d6c1b2cd/code-insider-x64-1743053042.tar.gz";
     src = fetchTarball {
-      url = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/87a35e10699cb846c2c72eeb3a698bf56dafd701/code-insider-x64-1742966545.tar.gz";
-      sha256 = "sha256-TZAiMDF3g18z7JS5gyLluYkKb7C1vQgfTTkzCfei77M=";
+      url = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/ff52a0da2eeb3d4c590a85cf14ca13a9d6c1b2cd/code-insider-x64-1743053042.tar.gz";
+      sha256 = "sha256-vojRj35RYCjhOi5YABFIcOW61U7EW9xTdpbk41cmTe0=";
     };
   };
   vscode-linux-stable = {
